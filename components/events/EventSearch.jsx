097b62ab0d6bc61db5cb10a701a7c2e5c9c14d7a
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-11-15 14:46:37 +0000

    started event search component

---
+++ components/events/EventSearch.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+//t0do: add button component
+import Button from '../ui/button';
+
+const EventsSearch = (props) => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="year">Search:</label>
+        <select id="year" onChange={props.handleChange}>
+          <option value="">Select Year</option>
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="month">Search:</label>
+        <select id="month" onChange={props.handleChange}>
+          <option value="1">January</option>
+          <option value="2">February</option>
+          <option value="3">March</option>
+          <option value="4">April</option>
+          <option value="5">May</option>
+          <option value="6">June</option>
+          <option value="7">July</option>
+          <option value="8">August</option>
+          <option value="9">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
+      
+        </select>
+      </div>
+      <Button type="submit" onClick={props.handleSubmit}>Search</Button>
+    </form>
+  )
+}
+
+export default EventsSearch
