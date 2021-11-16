02f8459ff35168e321f7f06a9e6a3eaebf784e5d
Alex macbookAir <aleksandrmisnov@gmail.com>
2021-11-16 14:10:30 +0000

    added layout

---
+++ components/layout/MainHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './MainHeader.module.css'
+
+const MainHeader = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+            Child Events
+        </Link>
+      </div>
+      <nav className={styles.navigation}>
+        <Link href="/events">Browse All Events</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default MainHeader
